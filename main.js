8341bd30015d77317a0bd6f32f1d961a33d34ebb
Jacob Reckhard <jacobreckhard@gmail.com>
2020-04-23 17:17:24 -0600

    added repl

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+import rl from 'readline-sync'
+
 import { parse } from "./src/core/parser.js"
 import { evaluate } from "./src/core/eval.js"
 import { init_runtime } from "./src/core/runtime.js"
 
-// TODO: make REPL
 init_runtime()
 
-evaluate(parse(`(def test_fn (fn (n) (fn () (set n (- n 1)) n)))`))
-evaluate(parse(`(def t1 (test_fn 5))`))
-console.log(evaluate(parse('(+ (t1) (t1))')))
+function repl () {
+  while (true) {
+    let line = rl.question('>>> ')
+    let parsed = parse(line)
+    let evaluated = evaluate(parsed)
+    console.log(evaluated)
+  }
+}
+
+repl()
